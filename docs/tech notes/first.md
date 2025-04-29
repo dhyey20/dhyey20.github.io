5c847605aa1ae7da0e55d9d4f4eaec8fcbf71778
dhyey20 <9337987+dhyey20@users.noreply.github.com>
2025-04-28 22:22:07 -0400

    moving to mkdocs

---
+++ docs/tech notes/first.md
@@ -0,0 +1,3 @@
+# First Blog
+
+This is a placeholder.
---
+++ scripts/serve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+# docker run -it -p 8000:8000 -v D:\dhyey20.github.io:/mkdocs  python:3.10 -- cd mkdocs; ./scripts/serve.sh
+
+python -m pip install --upgrade pip setuptools
+python -m pip install -r requirements.txt
+
+mkdocs serve -a 0.0.0.0:8000 -v
